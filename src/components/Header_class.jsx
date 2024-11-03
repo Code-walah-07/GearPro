36bf59a9846a9597f25914365fb768442b93f148
Deepak Chauhan <dchauhan2159@gmail.com>
2024-11-03 20:40:54 +0530

    file updated

--- src/components/Header_class.jsx
+++ src/components/Header_class.jsx
@@ -1,42 +1,80 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import {
+  faMagnifyingGlass,
+  faUser,
+  faCartShopping,
+  faRightToBracket,
+} from "@fortawesome/free-solid-svg-icons";
 
 function Header_class() {
   return (
-    <header class=" p-4 ">
-    <div class="bg-white h-10 flex text-wrap w-full  ">
-      <div class="w-2/6 ml-3 mr-4 ">
-          <a class="h-10" href="">
+    <header class=" p-1 h-18 flex flex-wrap">
+      <div class="bg-white h-16 flex text-wrap w-full  ">
+        <div class=" w-18 mr-2 ">
+          <a class="h-16   " href="">
             <img
-              class="h-10 "
+              class="h-10 w-14 mt-2"
               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScS7SdDZdR55dvs-eZpvewIRhtsDyodmTj_Q&s"
               alt="GearPro_Logo"
             />
           </a>
-      </div>
-      <div class="flex text-wrap w-5/6">
-        <div class="w-1/6 h-10 bg-slate-200 rounded-l-lg ">
-          <div class="text-center p-2">
-            <FontAwesomeIcon icon={faMagnifyingGlass} />
+        </div>
+        <div class="flex text-wrap w-3/6 ">
+          <div class="w-1/6 h-10 bg-slate-200 rounded-l-lg mt-2">
+            <div class="text-center p-2 w-5/6">
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </div>
+          </div>
+          <div class="w-5/6 >">
+            <input
+              class=" w-5/6 h-10 mt-2 rounded-r-lg text-xs bg-slate-200"
+              type="text"
+              placeholder="   Enter for Search"
+            />
           </div>
         </div>
-        <div class="w-5/6 >">
-          <input
-            class=" w-5/6 h-10 rounded-r-lg bg-slate-200"
-            type="text"
-            placeholder="   Enter for Search"
-          />
+        <div class=" h-10 w-3/6 mt-2 flex ">
+          <div class="h-10 w-2/6  flex flex-wrap ">
+            <a
+              class=" mt-1 ml flex-col w-2/6 hover:bg-blue-800 hover:text-white justify-center text-center"
+              href=""
+            >
+              <div class="text-xs pl-2 pr-2 ">
+                <FontAwesomeIcon icon={faRightToBracket} />
+              </div>
+              <div class="text-xs">
+                <b>Login</b>
+              </div>
+            </a>
+          </div>
+          <div class="h-10 w-2/6  flex flex-wrap">
+            <a
+              class=" mt-1 ml flex-col w-2/6 hover:bg-blue-800 hover:text-white justify-center text-center"
+              href=""
+            >
+              <div class="text-xs pl-2 pr-2 ">
+                <FontAwesomeIcon icon={faUser} />
+              </div>
+              <div class="text-xs">
+                <b>Profile</b>
+              </div>
+            </a>
+          </div>
+          <div class="h-10 w-2/6  flex flex-wrap">
+          <a
+              class=" mt-1 ml flex-col w-2/6 hover:bg-blue-800 hover:text-white justify-center text-center"
+              href=""
+            >
+              <div class="text-xs pl-2 pr-2 ">
+                <FontAwesomeIcon icon={faCartShopping} />
+              </div>
+              <div class="text-xs">
+                <b>Cart</b>
+              </div>
+            </a>
+          </div>
         </div>
       </div>
-      <div class=" w-2/6 mr-5 border-solid border-2 border-gray-600 rounded-md bg-red-600 hover:bg-red-400">
-        <a class="text-white" href="">
-          Login
-        </a>
-      </div>
-      <div class=" w-1/6 mr-10 rounded-md bg-blue-600 hover:bg-blue-400">
-        <a class="p-2 text-base text-white " href="">Cart</a>
-      </div>
-    </div>
     </header>
   );
 }
