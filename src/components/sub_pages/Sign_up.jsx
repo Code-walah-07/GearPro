18355848c3af5ffdd2f87b458e1b9d6d62d97dec
Deepak Chauhan <dchauhan2159@gmail.com>
2025-08-29 14:00:48 +0530

    Added

--- src/components/sub_pages/Sign_up.jsx
+++ src/components/sub_pages/Sign_up.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
